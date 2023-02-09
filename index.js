8d3024a9229c6191269692b7664ab16a5774d58d
Steve Villanueva <villa_se@yahoo.com>
2023-02-09 11:05:28 -0600

    commit tree 1a7c77169aefe70475127a6e473f032cef34a40b

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+//for (let age = 30; age < 40; age++) {
+//    console.log(`I'm ${age} years old. Happy birthday to me!`);
+//}
+
+//const gifts = ["teddy bear", "drone", "doll"];
+
+//function wrapGifts(gifts) {
+//    for (let i = 0; i < gifts.length; i++) {
+//        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//        debugger;
+//    }
+
+//    return gifts;
+//}
+
+//wrapGifts(gifts);
+
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards) {
+    for (let i = 0; i < cards.length; i++) {
+        console.log(`${cards[i]}, "surprise"`);
+    }
+
+    return cards;
+}
+
+wrapCards(cards)
